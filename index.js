9748088ea5a8defeb2cb5e73c7aa348449c23a0e
aderibigbeganiu <aderibigbeganiu@gmail.com>
2024-04-12 15:42:29 +0100

    initial commit

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ const promptText = require("./prompts");
 const tweet = require("./tweet");
 const schedule = require("node-schedule");
 
-const scheduledTask = schedule.scheduleJob("*/10 * * * *", () => {
-    console.log("Task executed every minute:", new Date().toLocaleTimeString());
-    tweet(promptText[0]);
-});
+try {
+    const scheduledTask = schedule.scheduleJob("*/5 * * * *", () => {
+        console.log(
+            "Task executed every minute:",
+            new Date().toLocaleTimeString()
+        );
+        tweet(promptText[0]);
+    });
+} catch (error) {
+    console.log(error);
+}
